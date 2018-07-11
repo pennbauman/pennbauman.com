3ea87015f4d41b40657ac0447ad1ded983b5459f
Penn Bauman <penn.bauman@gmail.com>
2018-07-11 00:52:23 -0400

    modified:   sys/test/index.php

--- sys/test/index.php
+++ sys/test/index.php
@@ -2,38 +2,41 @@
 	include $_SERVER["DOCUMENT_ROOT"]."/files/php/std.php";
 	include $_SERVER["DOCUMENT_ROOT"]."/files/php/auth.php";
 	include $_SERVER["DOCUMENT_ROOT"]."/files/php/file_path.php";
+
+	if ($auth > 8) {
+		echo "<!DOCTYPE html><head>";
+		echo "<title>Test</title>";
+		echo "<link rel='icon' href='/files/images/sys_favicon.png'>";
+		echo "<link rel='stylesheet' type='text/css' href='/files/css/backend.css'>";
+		echo "<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script>";
+		echo "<script src='/files/js/general.js'></script>";
+		echo "</head>\n<body>";
+
+		echo "user: ".$username." (<a href='/account/logout.php".$returnLink."'>logout</a>)";
+		echo "<h1>Test</h1>";
+
+		echo "<p>h1</p>";
+		$array1 = ["one", "two", "three"];
+		$array2 = ["", "one", "two"];
+		$array3 = [null, "one", "two"];
+		echo "1:".implode(" | ", $array1)."<br/>";
+		echo "2:".implode(" | ", $array2)."<br/>";
+		echo "3:".implode(" | ", $array3)."<br/>";
+		
+		echo "<select id='pstat1' class='pstat'>";
+		echo "<option value='8'>8</option>";
+		echo "<option value='9'>9 (-1)</option>";
+		echo "<option value='10'>10 (-2)</option>";
+		echo "<option value='11'>11 (-3)</option>";
+		echo "<option value='12'>12 (-4)</option>";
+		echo "<option value='13'>13 (-5)</option>";
+		echo "<option value='14'>14 (-7)</option>";
+		echo "<option value='15'>15 (-9)</option>";
+		echo "</select>";
+
+		echo '<br/><br/><br/><a href="/">Home</a> - <a href="/sys/">System</a>';
+		echo "</body></html>";
+	} else {
+		include $_SERVER["DOCUMENT_ROOT"]."/files/php/auth_error.php";
+	}
 ?>
-<!DOCTYPE html>
-	<head>
-		<title>Test | Penn Bauman</title>
-		<link rel='icon' href='/files/images/sys_favicon.png'>
-		<link rel='stylesheet' type='text/css' href='/files/css/backend.css'>
-		<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script>
-		<script src='/files/js/general.js'></script>
-	</head>
-		<?php 
-			echo "user: ".$username." (<a href='/account/logout.php".$returnLink."'>logout</a>)";
-			echo "<h1>Test</h1>";
-			
-			echo "<p>h1</p>";
-			$array1 = ["one", "two", "three"];
-			$array2 = ["", "one", "two"];
-			$array3 = [null, "one", "two"];
-			echo "1:".implode(" | ", $array1)."<br/>";
-			echo "2:".implode(" | ", $array2)."<br/>";
-			echo "3:".implode(" | ", $array3)."<br/>";
-			
-			echo "<select id='pstat1' class='pstat'>";
-			echo "<option value='8'>8</option>";
-			echo "<option value='9'>9 (-1)</option>";
-			echo "<option value='10'>10 (-2)</option>";
-			echo "<option value='11'>11 (-3)</option>";
-			echo "<option value='12'>12 (-4)</option>";
-			echo "<option value='13'>13 (-5)</option>";
-			echo "<option value='14'>14 (-7)</option>";
-			echo "<option value='15'>15 (-9)</option>";
-			echo "</select>";
-		?>
-		<br/><br/><a href="/">Home</a> - <a href="/sys/">System</a>
-	</body>
-</html>
