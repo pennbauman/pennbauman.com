2bc2a1cd0421789270191ee501e67ff9542e8183
Penn Bauman <penn.bauman@gmail.com>
2018-07-11 04:17:46 -0400

    modified:   .gitignore modified:   files/php/std_dir_page.php modified:   index.php new file:   pgm/monte-carlo/dir.php new file:   pgm/morse-code/dir.php new file:   pgm/periodic-table/dir.php new file:   pgm/sexy-dice/dir.php

--- files/php/std_dir_page.php
+++ files/php/std_dir_page.php
@@ -2,26 +2,9 @@
 	include $_SERVER["DOCUMENT_ROOT"]."/files/php/std.php";
 	include $_SERVER["DOCUMENT_ROOT"]."/files/php/auth.php";
 	include $_SERVER["DOCUMENT_ROOT"]."/files/php/file_path.php";
-	/*$page = file_get_contents("page.md");
-	$page = explode("\n", $page);
-	$i = 9;
-	$page_text = "";
-	while ($i < count($page)) {
-		$page_text = $page_text.$page[$i]."\n";
-		$i++;
-	} //*/
-	/*function arrayPrint($array, $spacer) {
-		$i = 0;
-		$fin = "";
-		while ($i < count($array)) {
-			$fin = $fin.$array[$i].$spacer;
-			$i++;
-		}
-		return $fin;
-	} //*/
-	//$filePath = $filePath;
-	// Print Head
+	
 	if ($auth > 8) {
+		// Print Head
 		echo "<!DOCTYPE html><head>";
 		echo "<title>".$filePathShort."/"."</title>";
 		echo "<link rel='icon' href='/files/images/files_favicon.png'>";
@@ -33,9 +16,8 @@
 		//Print Content
 		echo "user: ".$username." (<a href='/account/logout.php".$returnLink."'>logout</a>)";
 		echo "<h1>".$filePathShort."/"."</h1>";
+
 		$files = scandir($filePath);
-		//echo arrayPrint($files, "<br/>\n");
-		
 		echo "<p><a href='";
 		$i = 0;
 		while ($i < count($filePathArray) - 1) {
--- index.php
+++ index.php
@@ -2,10 +2,12 @@
 	include $_SERVER["DOCUMENT_ROOT"]."/files/php/std.php";
 	include $_SERVER["DOCUMENT_ROOT"]."/files/php/auth.php";
 	include $_SERVER["DOCUMENT_ROOT"]."/files/php/file_path.php";
+
 	$link = URLVar("l");
 	if ($link == "dnd5ebooks") {
 		header("Location: https://drive.google.com/drive/folders/1hVgL3hpDhLY2H0Fh4rHtmMPN1TO9Ex81");
 	}
+	
 ?>
 <html> 
 	<head>
