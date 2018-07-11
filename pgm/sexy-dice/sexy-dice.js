abcb7f3dc646037c70f9c896f4ffeab1fdacc480
Penn Bauman <penn.bauman@gmail.com>
2018-07-11 02:59:43 -0400

    modified:   files/images/morse_favicon.png modified:   pgm/sexy-dice/index.html new file:   pgm/sexy-dice/sexy-dice.css new file:   pgm/sexy-dice/sexy-dice.js

---
+++ pgm/sexy-dice/sexy-dice.js
@@ -0,0 +1,47 @@
+var text1 = new Array(
+	"kiss",
+	"rub",
+	"grab",
+	"poke",
+	"hug",
+	"suck",
+	"nibble on",
+	"spank",
+	"lick",
+	"touch",
+	"blow on",
+	"caress",
+	"touch"
+);
+var text2 = new Array(
+	"my crotch",
+	"my ass",
+	"my face",
+	"my toes",
+	"my chest", 
+	"my neck",
+	"my navel",
+	"my hand",
+	"my ears",
+	"my thighs",
+	"my lips",
+	"my nipples"
+);
+function setup() {
+	//alert("javascript running");
+	return false;
+}
+function roll() {
+	//alert("roll");
+	print("d1", text1[randInt(text1.length-1)]);
+	print("d2", text2[randInt(text2.length-1)]);
+	return false;
+}
+function randInt(max) {
+	var fin = Math.floor((Math.random() * max) + 1); 
+	return fin;
+} //*/
+function print(loc, text) {
+  document.getElementById(loc).innerHTML = text;
+  return false;
+}
