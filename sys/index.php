b21640df46abab0c25a6e15e153d02b5094b6836
Penn Bauman <penn.bauman@gmail.com>
2018-07-14 01:23:22 -0400

    modified:   sys/index.php renamed:    sys/php/documentation/index.php -> sys/php/doc/index.php

--- sys/index.php
+++ sys/index.php
@@ -20,6 +20,7 @@
 		echo "<a href='/sys/test/'>Test</a> <br/>";
 		echo "<a href='/sys/test/favicon/'>Favicon Tester</a> <br/>";
 		echo "<a href='/sys/test/color/'>Color Tester</a> <br/>";
+		echo "<a href='/sys/php/'>PHP</a> <br/>";
 		echo "</p>";
 		echo '<br/><a href="/">Home</a>';
 		echo "</body></html>";
