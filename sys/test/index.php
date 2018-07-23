6e572e7e40772974c90b220713637b313650fd06
Penn Bauman <penn.bauman@gmail.com>
2018-07-23 16:38:47 -0400

    modified:   sys/test/index.php

--- sys/test/index.php
+++ sys/test/index.php
@@ -5,16 +5,16 @@
 
 	if ($auth > 8) {
 		echo "<!DOCTYPE html><head>";
-		echo "<title>System</title>";
+		echo "<title>Test</title>";
 		echo "<link rel='icon' href='/files/images/sys_favicon.png'>";
 		echo "<link rel='stylesheet' type='text/css' href='/files/css/backend.css'>";
 		echo "<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script>";
 		echo "<script src='/files/js/general.js'></script>";
-		// Print Body
 		echo "</head>\n<body>";
-		//Print Content
+		
 		echo "user: ".$username." (<a href='/account/logout.php".$returnLink."'>logout</a>)";
-		echo "<h1>System</h1>";
+		echo "<h1>Test</h1>";
+
 		echo "<p>";
 		echo "<a href='/sys/hash/'>Hash</a> <br/>";
 		echo "<a href='/sys/page-size/'>Page Size</a> <br/>";
@@ -24,7 +24,8 @@
 		echo "<a href='/sys/php/'>PHP</a> <br/>";
 		echo "<a href='/sys/php/errors/'>PHP Errors</a> <br/>";
 		echo "</p>";
-		echo '<br/><a href="/">Home</a>';
+		
+		echo '<br/><br/><br/><a href="/">Home</a> - <a href="/sys/">System</a>';
 		echo "</body></html>";
 	} else {
 		include "/home/valypfnd/php/auth_error.php";
