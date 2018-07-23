b4ce407397484d4fc47257deb7e3c20bde899ca0
Penn Bauman <penn.bauman@gmail.com>
2018-07-23 16:39:07 -0400

    modified:   sys/test/index.php

--- sys/test/index.php
+++ sys/test/index.php
@@ -2,6 +2,7 @@
 	include "/home/valypfnd/php/std.php";
 	include "/home/valypfnd/php/auth.php";
 	include "/home/valypfnd/php/file_path.php";
+	include "/home/valypfnd/php/get_site.php";
 
 	if ($auth > 8) {
 		echo "<!DOCTYPE html><head>";
