969ebb8040a68d45f16fbfb35e026dd4111dedfe
Penn Bauman <penn.bauman@gmail.com>
2018-07-23 10:32:43 -0400

    modified:   sys/test/index.php

--- sys/test/index.php
+++ sys/test/index.php
@@ -21,24 +21,8 @@
 		echo $currentSite;
 		echo "<br/><br/>";
 
-		echo "<p>h1</p>";
-		$array1 = ["one", "two", "three"];
-		$array2 = ["", "one", "two"];
-		$array3 = [null, "one", "two"];
-		echo "1:".implode(" | ", $array1)."<br/>";
-		echo "2:".implode(" | ", $array2)."<br/>";
-		echo "3:".implode(" | ", $array3)."<br/>";
-		
-		echo "<select id='pstat1' class='pstat'>";
-		echo "<option value='8'>8</option>";
-		echo "<option value='9'>9 (-1)</option>";
-		echo "<option value='10'>10 (-2)</option>";
-		echo "<option value='11'>11 (-3)</option>";
-		echo "<option value='12'>12 (-4)</option>";
-		echo "<option value='13'>13 (-5)</option>";
-		echo "<option value='14'>14 (-7)</option>";
-		echo "<option value='15'>15 (-9)</option>";
-		echo "</select>";
+		echo $_SERVER['HTTP_HOST'];
+		echo $_SERVER['REQUEST_URI'];
 
 		echo '<br/><br/><br/><a href="/">Home</a> - <a href="/sys/">System</a>';
 		echo "</body></html>";
