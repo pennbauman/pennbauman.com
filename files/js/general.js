5d2c80526c389620b026909c218e696e31844961
Penn Bauman <penn.bauman@gmail.com>
2018-07-28 18:27:25 -0400

    modified:   files/css/_general.scss modified:   files/css/general.css modified:   files/js/general.js modified:   index.php

--- files/js/general.js
+++ files/js/general.js
@@ -1,3 +1,7 @@
+var script = document.createElement('script');
+script.src = '//ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js';
+document.getElementsByTagName('head')[0].appendChild(script); 
+
 $(document).ready(function() { 
 	$(':root').css("font-size", Math.max(screen.height, window.innerHeight)*0.01);
 });
--- index.php
+++ index.php
@@ -21,7 +21,7 @@
 		<title>Penn Bauman</title>
 		<link rel='icon' href='/files/images/favicon.png'>
 		<link rel='stylesheet' type='text/css' href='/files/general.css'>
-		<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script>
+		<!--script src='https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script-->
 		<script src='/files/general.js'></script>
 	</head>
 	<body>
