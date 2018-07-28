d15958d6f2d6f41cbbba625e44ccdfc8b1aa6eef
Penn Bauman <penn.bauman@gmail.com>
2018-07-28 18:31:52 -0400

    modified:   files/css/general.css modified:   files/js/general.js

--- files/js/general.js
+++ files/js/general.js
@@ -1,6 +1,7 @@
-var script = document.createElement('script');
+/*var script = document.createElement('script');
 script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js';
-document.getElementsByTagName('head')[0].appendChild(script); 
+document.getElementsByTagName('head')[0].appendChild(script);  //*/
+document.head.innerHTML += "<script src=\"/path/to/jquery.js\"></script>";
 
 $(document).ready(function() { 
 	$(':root').css("font-size", Math.max(screen.height, window.innerHeight)*0.01);
