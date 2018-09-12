b5ccfd12cbfa55480514947d8052b785d4657b27
Penn Bauman <penn.bauman@gmail.com>
2018-09-12 01:55:44 -0400

    num highlights lanthanide and actinide

--- projects/periodic-table/periodic-table.js
+++ projects/periodic-table/periodic-table.js
@@ -242,13 +242,15 @@ $(document).ready(function () {
   $('.elem_row').find('.std_block').not(':first-child').on('mouseenter mouseleave', function () {
     var i = $(this).index();
 	if ($(this).hasClass("lanthanide")) {
-		//getElementById("row6").toggleClass("num_hl");
+		getElementById("row6").toggleClass("num_hl");
 	} else if ($(this).hasClass("actinide")) {
-		//getElementById("row7").toggleClass("num_hl");
+		getElementById("row7").toggleClass("num_hl");
+	} else {
+		$(this).closest('tr').find('td').first().toggleClass('num_hl');
 	}
 
     //$(this).toggleClass('bg');
-    $(this).closest('tr').find('td').first().toggleClass('num_hl');
+
     $('.tops_nums td').eq(i).toggleClass('num_hl');
   });
 });
