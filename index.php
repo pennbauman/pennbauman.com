003ab8ceac54cc6b4309041dc4c05855d49a454f
Penn Bauman <penn.bauman@gmail.com>
2019-01-21 22:22:23 -0500

    switched back to php header and footer

--- index.php
+++ index.php
@@ -26,17 +26,8 @@
 	</head>
 	<body>
 		<?php
-			//include "/home/valypfnd/php/pennbauman/std_header_navbar.php";
+			include "/home/valypfnd/php/pennbauman/std_header_navbar.php";
 		?>
-		<div id='header'>
-			<a href='/'><h1>Penn Bauman</h1></a>
-			<div id='navbar'>
-				<a href='/resume/'>Resume</a>
-				<a href='/story/'>My Story</a>
-				<a href='/projects/'>Projects</a>
-				<a href='http://github.com/pennbauman'>GitHub</a>
-			</div>
-		</div>
 		<div id="content">
 			<!--img src="/files/images/bella_wag.gif"/-->
 			<?php 
@@ -61,16 +52,9 @@
 					<td><a href="/projects/">Projects</a></td>
 				<tr>
 			</table-->
-			<p> 
-				Email: <a mailto='mailto:penn.bauman@gmail.com'>penn.bauman@gmail.com</a> <br/>
-				Phone: <a href='tel:703-963-2729'>703-963-2729</a>
-			</p>
-			<p>
-				<span style='font-size:1.4em'>&copy;</span> 2018 Penn Bauman
-			</p>
 		</div>
 		<?php
-			//include "/home/valypfnd/php/pennbauman/std_footer.php";
+			include "/home/valypfnd/php/pennbauman/std_footer.php";
 		?>
 	</body>
 </html>
