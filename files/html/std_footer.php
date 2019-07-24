cc98526866195b821ec7d40d06703a7c09aaf9a6
Penn Bauman <penn.bauman@gmail.com>
2019-07-23 21:39:48 -0400

    added header and footer html files

---
+++ files/html/std_footer.php
@@ -0,0 +1,7 @@
+<div id='footer'>
+	<p>
+		Email: <a mailto='mailto:pennbauman@protonmail.com'>pennbauman@protonmail.com</a> <br/>
+		Phone: <a href='tel:703-963-2729'>703-963-2729</a>
+	</p>
+	<p><span style='font-size:1.4em'>&copy;</span> 2019 Penn Bauman</p>
+</div>
---
+++ files/html/std_header_navbar.php
@@ -0,0 +1,9 @@
+<div id='header'>
+	<a href='/'><h1>Penn Bauman</h1></a>
+	<div id='navbar'>
+		<a href='/resume'>Resume</a>
+		<!--a href='/about/'>About</a-->
+		<a href='/projects'>Projects</a>
+		<a href='http://github.com/pennbauman'>GitHub</a>
+	</div>
+</div>
