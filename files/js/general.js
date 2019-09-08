011a75c6d7e40160b1ecf66503b169f1e199ec92
Penn Bauman <pennbauman@protonmail.com>
2019-09-08 18:37:53 -0400

    fixing backend text area format

--- files/js/general.js
+++ files/js/general.js
@@ -1,11 +1,12 @@
 $(document).ready(function() { 
 	$(':root').css("font-size", Math.max(screen.height, window.innerHeight)*0.01);
 });
-$('textarea').on('paste input', function () {
-    if ($(this).outerHeight() > this.scrollHeight){
-        $(this).height(1)
-    }
-    while ($(this).outerHeight() < this.scrollHeight + parseFloat($(this).css("borderTopWidth")) + parseFloat($(this).css("borderBottomWidth"))){
-        $(this).height($(this).height() + 1)
-    }
-});
+//$('textarea').on('paste input', function () {
+    //if ($(this).outerHeight() > this.scrollHeight){
+        //$(this).height(1)
+    //}
+    //while ($(this).outerHeight() < this.scrollHeight + parseFloat($(this).css("borderTopWidth")) + parseFloat($(this).css("borderBottomWidth"))){
+        //$(this).height($(this).height() + 1)
+    //}
+//});
+$('#textMeetingAgenda').css('overflow', 'hidden').autogrow();
