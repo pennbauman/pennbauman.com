3cfddfcdc861a9207912b3d15a01bb7fba7edd82
Penn Bauman <pennbauman@protonmail.com>
2019-09-08 21:22:44 -0400

    finalized text edit

--- files/js/general.js
+++ files/js/general.js
@@ -1,4 +1,3 @@
-//import * from "https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js";
 function includeJs(jsFilePath) {
     var js = document.createElement("script");
 
@@ -11,6 +10,7 @@ function includeJs(jsFilePath) {
 $(document).ready(function() { 
 	$(':root').css("font-size", Math.max(screen.height, window.innerHeight)*0.01);
 });
+
 function textareaSize() {
 	$("textarea").each(function(){
 		this.style.height = "0";
--- files/js/textarea_resize.js
+++
@@ -1,7 +0,0 @@
-//includeJs("/files/js/autoresize_jquery.js");
-//$('#textarea').autoResize();
-$(".autoExpand").keyup(function(e) {
-    while($(this).outerHeight() < this.scrollHeight + parseFloat($(this).css("borderTopWidth")) + parseFloat($(this).css("borderBottomWidth"))) {
-        $(this).height($(this).height()+1);
-    };
-});
