09bf06803737fc67db93519cfa35f5617ea66bcc
Penn Bauman <pennbauman@protonmail.com>
2019-09-08 20:29:48 -0400

    fixing backend text area format

--- files/js/textarea_resize.js
+++ files/js/textarea_resize.js
@@ -1,5 +1,5 @@
 //includeJs("/files/js/autoresize_jquery.js");
-$('#textarea').autoResize();
+//$('#textarea').autoResize();
 $(".autoExpand").keyup(function(e) {
     while($(this).outerHeight() < this.scrollHeight + parseFloat($(this).css("borderTopWidth")) + parseFloat($(this).css("borderBottomWidth"))) {
         $(this).height($(this).height()+1);
