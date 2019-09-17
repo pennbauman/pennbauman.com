bfbcc9ccad65831c947d22caa24e61b815de019b
Penn Bauman <pennbauman@protonmail.com>
2019-09-16 22:12:15 -0400

    fixing txt not resizing on load

--- files/js/general.js
+++ files/js/general.js
@@ -9,6 +9,7 @@ function includeJs(jsFilePath) {
 //includeJs("https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js");
 $(document).ready(function() { 
 	$(':root').css("font-size", Math.max(screen.height, window.innerHeight)*0.01);
+	textareaSize();
 });
 
 function textareaSize() {
