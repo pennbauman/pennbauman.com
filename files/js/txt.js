217e9420f8288fba397fc0125b1d31637c79ed46
Penn Bauman <pennbauman@protonmail.com>
2019-11-09 02:35:03 -0500

    set up txt

--- files/js/txt.js
+++ files/js/txt.js
@@ -2,13 +2,7 @@ function txtUpdate(file, pass) {
 	var body = document.getElementById("textarea").innerHTML;
 	var url = "/txt?f=" + file + "&pass=" + pass + "&body=" + body + "&js=set";
 	xmlhttp=new XMLHttpRequest();
-	xmlhttp.onreadystatechange=function() {
-		if (xmlhttp.readyState==4 && xmlhttp.status==200) {
-			return xmlhttp.responseText;
-		}
-	}
-	xmlhttp.open("GET", url, false );
-	xmlhttp.send();
+	xmlhttp.open("GET", url, true);
 
 	return false;
 }
