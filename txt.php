734e50806770b95aee8c7b8e00aa82c403adf9b5
Penn Bauman <pennbauman@protonmail.com>
2019-11-08 23:50:09 -0500

    working on txt

--- txt.php
+++ txt.php
@@ -16,10 +16,20 @@
 				header("Location: /txt");
 				exit();
 			}
+			$txtAuth = false;
 			if ($result["password"] == "") {
 				$txtAuth = true;
 			} else {
-				$txtAuth = false;
+				if (isset($_POST["password"])) {
+					$txtPass = hash("sha512", $_POST["password"]);
+					if ($txtPass = $result["password"]) {
+						$txtAuth = true;
+					}
+				}
+			}
+			if ($txtAuth == false) {
+				header("Location: /txt");
+				exit();
 			}
 		} else {
 			header("Location: /txt");
@@ -50,7 +60,11 @@
 				echo "<input type='hidden' name='password' value='$password'>";
 				echo "<input type='submit' value='Save and Enter'></form>";
 			} else {
-				echo "unauthorized";
+				echo "<form class='wide' action='".$_SERVER['REQUEST_URI']."' method='post'>";
+				echo "<h4>Password:</h4><br/> <input type='password' name='password'> <br/>";
+				echo "<h6 class='error'>".$passError."</h6> <br/><br/>";
+				echo "<input type='submit' value='Login'>";
+				echo "</form>";
 			}
 		?>
 	</body>
