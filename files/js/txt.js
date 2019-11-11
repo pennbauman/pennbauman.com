86d51de76da6b44676da126b4588bffafac06054
Penn Bauman <pennbauman@protonmail.com>
2019-11-11 01:23:45 -0500

    setting txt save speed

--- files/js/txt.js
+++ files/js/txt.js
@@ -6,7 +6,7 @@ function txtReady(file, pass) {
 	txtFile = file;
 	txtPass = pass;
 	//console.log("f: " + file + " p: " + pass);
-	setInterval(function(){ txtUpdate(false); }, 500);
+	setInterval(function(){ txtUpdate(false); }, 1000);
 	document.getElementById("textarea").addEventListener("click", function(){ changed = true });
 }
 
