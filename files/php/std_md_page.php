9f5fa0f03ee537eb7ade94c49d2e9f2b26d8e0da
Penn Bauman <pennbauman@protonmail.com>
2019-12-22 04:00:37 -0500

    testing custom md page

--- files/php/std_md_page.php
+++ files/php/std_md_page.php
@@ -17,11 +17,11 @@
 	while ($i < count($page)) {
 		if (($i == 0) && preg_match("(-)+", $page[$i])) {
 			$inMeta = true;
+		} else if ($inMeta && preg_match("[-]+", $page[$i])) {
+			$inMeta = false;
 		} else if ($inMeta) {
 			$line_split = explode($page[$i], ":");
 			$page_meta[$line_split[0]] = $line_split[1];
-		} else if ($inMeta && preg_match("[-]+", $page[$i])) {
-			$inMeta = false;
 		} else {
 			$page_text = $page_text.$page[$i]."\n";
 		}
