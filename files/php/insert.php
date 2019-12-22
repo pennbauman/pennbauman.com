2fcd98c20426cc3c7e753f63af9ec1a9f712828d
Penn Bauman <pennbauman@protonmail.com>
2019-12-22 03:15:48 -0500

    testing custom md page

--- files/php/insert.php
+++ files/php/insert.php
@@ -8,9 +8,9 @@
 		echo $Parsedown->text($text);
 	} //*/
 
+	/*
 	function insertHTML($file) {
 		$text = file_get_contents($_SERVER["DOCUMENT_ROOT"]."/files/html/".$file.".html");
 		echo $text;
-	}
-
+	} //*/
 ?>
