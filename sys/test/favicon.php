f34c255bdd6a8f4bfa799ca578c6a3059a22f259
Penn Bauman <pennbauman@protonmail.com>
2019-12-23 15:28:12 -0500

    updating sys pages

--- sys/test/favicon.php
+++ sys/test/favicon.php
@@ -16,10 +16,17 @@
 		<link rel='stylesheet' type='text/css' href='/files/css/backend.css'>
 		<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script>
 		<script src='/files/js/general.js'></script>
+		<script>
+			function setFavicon() {
+				$("#favicon").attr("href", document.getElementById("url").value);
+				return false;
+			}
+
+		</script>
 	</head>
 	<body>
 		<h1>Favicon Tester</h1>
-		<form action='/sys/test/favicon/' method='get'>
+		<form onsubmit="setFavicon();">
 			<b>Favicon URL:</b> <br/> 
 			<input autofocus type='text' name='url' value='/files/img/'> <br><br/>
 			<input type='submit' value='Enter'>
