b94789c16cc1c519b5c1c6f54d51f338704eed53
Penn Bauman <pennbauman@protonmail.com>
2019-12-23 03:12:25 -0500

    updating link backend

--- sys/php-list.php
+++ sys/php-list.php
@@ -1,17 +1,15 @@
 <?php
-	include "/home/valypfnd/php/std.php";
+	//include "/home/valypfnd/php/std.php";
 	include "/home/valypfnd/php/auth.php";
-	include "/home/valypfnd/php/login_url.php";
+	//include "/home/valypfnd/php/login_url.php";
 	
 	if ($auth > 9) {
-		// Print Head
 		echo "<!DOCTYPE html><head>";
 		/echo "<title>PHP List</title>";
 		echo "<link rel='icon' href='/files/img/files_favicon.png'>";
 		echo "<link rel='stylesheet' type='text/css' href='/files/css/backend.css'>";
 		echo "<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script>";
 		echo "<script src='/files/js/general.js'></script>";
-		// Print Body
 		echo "</head>\n<body>";
 		//Print Content
 		echo "user: ".$username." (<a href='".$logoutURL."'>logout</a>)";
