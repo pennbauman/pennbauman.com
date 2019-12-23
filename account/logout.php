da21204ada9da4aeccbb5fdd16d81d5e5ab36b32
Penn Bauman <pennbauman@protonmail.com>
2019-12-23 14:47:49 -0500

    updating login/out

--- account/logout.php
+++ account/logout.php
@@ -3,8 +3,8 @@
 	//include "/home/valypfnd/php/auth.php";
 	//include "/home/valypfnd/php/subsite_return.php";
 	
-	setcookie('pb_username', '', time() - 3600, '/', "pennbauman.com");
-	setcookie('pb_password', '', time() - 3600, '/', "pennbauman.com");
+	setcookie('pb_username', '', time() - 3600, '/', ".pennbauman.com");
+	setcookie('pb_password', '', time() - 3600, '/', ".pennbauman.com");
 
 	//header("Location: ".$subsiteReturn);
 	header("Location: ".$sys['link']['return_url']);
