dc9b0487264248bed737fd3ad2cf57b51060d01c
Penn Bauman <pennbauman@protonmail.com>
2019-12-23 20:20:32 -0500

    cleaning comments

--- files/php/insert.php
+++ files/php/insert.php
@@ -6,10 +6,8 @@
 
 		function insertMD($file) {
 			$Parsedown = new Parsedown();
-			//$text = file_get_contents($_SERVER["DOCUMENT_ROOT"]."/files/md/".$file.".md");
 			echo $Parsedown->text(file_get_contents($_SERVER["DOCUMENT_ROOT"]."/files/md/".$file.".md"));
-			//echo $Parsedown->text($file);
-		} //*/
+		}
 
 		function insertHTML($file) {
 			echo file_get_contents($_SERVER["DOCUMENT_ROOT"]."/files/html/".$file.".html");
