d17211cbe422e7f82279240e883dec7a040dbb08
Penn Bauman <pennbauman@protonmail.com>
2019-12-25 01:29:47 -0500

    testing better dir page file hiding

--- files/php/dir_page.php
+++ files/php/dir_page.php
@@ -22,11 +22,14 @@
 		echo "dir'>parent directory</a></p><p><h4>content</h4></br>";
 		$files = scandir(getcwd());
 		for ($i = 0; $i < count($files); $i++) {
+			/*
 			if ($files[$i] != "."
 				&& $files[$i] != ".."
 				&& substr($files[$i], 0, 1) != "."
 				&& $files[$i] != "dir.php"
 				&& $files[$i] != "error_log") {
+			//*/
+			if (!preg_match("^\.", $files[$i])) {
 
 				echo "<a href='".substr($_SERVER['PHP_SELF'], 0, -8).$filePathShort."/".$files[$i];
 				if (is_dir($files[$i])) {
