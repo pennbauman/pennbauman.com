8baa343f7b7b306e6de0a0f2b7c4aa672a823b05
Penn Bauman <pennbauman@protonmail.com>
2019-12-25 02:06:45 -0500

    adding dnd to main site

---
+++ dnd.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html><html>
+	<head>
+		<title>Penn's D&amp;D</title>
+		<link rel='icon' href='/files/img/favicon.png'>
+		<link rel='stylesheet' type='text/css' href='/files/css/general.css'>
+		<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script>
+		<script src='/files/general.js'></script>
+	</head>
+	<body>
+		<?php insertHTML("dnd_header") ?>
+		<div id="content">
+			<h1>Penn Bauman's Dungeons &amp; Dragons</h1>
+			<h3><a href="/dice/">Dice</a></h3>
+			<p>Simple digital dice roller. Just enter the dice to be rolled in standard D&amp;D format and it will calculate their total for you.</p>
+			<h3><a href="/stats/">Stats</a></h3> 
+			<p>Quick D&amp;D statistic generator. Instantly generate a full set of statistics. Standard array, 1d20, best 3 of 4d6, and point buy avaiable. </p>
+			<h3><a href="https://docs.google.com/document/d/1Wy6KbhP3kRMCamv3M3PwnYH5qybheTDGk86MhuU_uyk/edit?usp=sharing">Character Sheet Template</a></h3>
+			<p>Basic Google Document template for a on line character sheet. Simple text document format allows for use of mobile and plenty of space for character details.</p>
+		</div>
+		</div>
+		<?php insertHTML("footer") ?>
+	</body>
+</html>
