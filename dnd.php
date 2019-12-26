cceefc7cf785c421a74f6a48d8667d4b70dd0c14
Penn Bauman <pennbauman@protonmail.com>
2019-12-26 03:11:37 -0500

    updating style

--- dnd.php
+++ dnd.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html><html>
 	<head>
 		<title>Penn's D&amp;D</title>
-		<link rel='icon' href='/files/img/dnd_favicon.png'>
+		<link rel='icon' href='/files/img/favicon.png'>
 		<link rel='stylesheet' type='text/css' href='/files/css/general.css'>
 		<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script>
 		<script src='/files/general.js'></script>
