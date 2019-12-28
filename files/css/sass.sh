4ce6f36d4f817da3017e208bc6842ef0ee39eac5
Penn Bauman <pennbauman@protonmail.com>
2019-12-28 01:53:28 -0500

    adding more dnd pages

--- files/css/sass.sh
+++ files/css/sass.sh
@@ -3,6 +3,12 @@
 if [[ $1 == back ]]; then
 	echo "backend.css"
 	sass backend.scss backend.css --no-source-map --watch
+elif [[ $1 == "dice" ]]; then
+	echo "dice.css"
+	sass dice.scss dice.css --no-source-map --watch
+elif [[ $1 == "stats" ]]; then
+	echo "stats.css"
+	sass stats.scss stats.css --no-source-map --watch
 else
 	echo "general.css"
 	sass hub.scss general.css --no-source-map --watch
