89455a124d00746dfebb255c17f9f748cd4e825e
Penn Bauman <pennbauman@protonmail.com>
2020-01-05 04:09:21 -0500

    updated includer

--- files/php/includer.php
+++ files/php/includer.php
@@ -1,6 +1,8 @@
 <?php
 /* Setup for Including
 	also includes std.php
+
+location $HOME/php/includer.php
  */
 	ini_set('include_path', $_SERVER['DOCUMENT_ROOT']."/files/php/");
 	include_once 'std.php';
