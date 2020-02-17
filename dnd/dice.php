29e18a7e18a2d4dd45a57532402127985c599cd4
Penn Bauman <pennbauman@protonmail.com>
2020-02-17 00:36:54 -0500

    overhauling look

--- dnd/dice.php
+++ dnd/dice.php
@@ -9,7 +9,9 @@
 		<script src='/files/js/dice.js'></script>
 	</head>
 	<body onload="return false;">
-		<?php insertHTML("dnd_header") ?>
+		<?php
+			insertHTML("dnd_header");
+		?>
 		<div id="content" class='dnd-content'>
 			<h1>D&amp;D Dice Roller</h1>
 			<table><tbody>
@@ -48,7 +50,7 @@
 		</div>
 		<?php
 			insertHTML("dnd_footer");
-			echo "footer"
+			echo "footer";
 		?>
 	</body>
 </html>
