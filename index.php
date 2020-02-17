4b73abba96bef634d3d5383331919acbfb7e79e1
Penn Bauman <pennbauman@protonmail.com>
2020-02-17 00:59:08 -0500

    overhauling look

--- index.php
+++ index.php
@@ -14,11 +14,13 @@
 		<meta charset="UTF-8">
 		<link rel='icon' href='/files/img/favicon.png'>
 		<!--link rel='stylesheet' type='text/css' href='/files/css/general.css'-->
+		<link rel='stylesheet' type='text/css' href='/files/font/DejaVu_Font.css'>
 		<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script>
 		<script src='/files/js/general.js'></script>
 		<style>
-			/* Prevent Firefox selected button/link outline */
-			:focus {outline:none;}
+			@import url('/files/font/DejaVu_Font.css');
+				/* Prevent Firefox selected button/link outline */
+				:focus {outline:none;}
 			::-moz-focus-inner {border:0;}
 			/* Prevent iOS input styling */
 			input.text, input[type="text"], input[type="button"], input[type="submit"], .input-checkbox {
@@ -29,14 +31,16 @@
 				font-size:1vh;
 			}
 			body {
+				margin:0;
+				padding:0;
+				min-height:100vh;
+
 				background-color:#141a1b;
 				background-image:url("/files/img/background.jpg");
 				background-position: center;
 				background-size: cover;
 				background-repeat: no-repeat;
 				background-attachment: fixed;
-				margin:0;
-				padding:0;
 
 				display:grid;
 				grid-template-areas:
@@ -45,15 +49,16 @@
 				grid-gap:0;
 				grid-template-columns:100%;
 				grid-template-rows:auto min-content;
+
+				text-align:center;
+				font-family:DejaVu_Sans_Mono, Monospace;
+				color:#EEEEEE;
 			}
 			#content {
 				position:fixed;
 				top:15rem;
 				left:5%;
 				width:90%;
-				text-align:center;
-				color:#141a1b;
-				color:#EEEEEE;
 				font-family:Monospace;
 			}
 			#content img {
@@ -88,6 +93,29 @@
 			i {
 				font-size:2.0rem;
 				font-style:italic;
+			}
+
+			#footer {
+				grid-area:footer;
+				display:block;
+				z-index:2;
+
+				width:100%;
+				height:2rem;
+				padding:1rem 0 1.5rem;
+
+				text-align:center;
+
+				color:$color-light;
+
+				p {
+					margin:0;
+				}
+
+				padding:1rem 0 1rem;
+				line-height:1.5rem;
+				font-size:1.5rem;
+				padding:1rem 0 1.5rem;
 			}
 		</style>
 	</head>
