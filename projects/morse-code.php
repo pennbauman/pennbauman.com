af7b4ef48781aaaa890a6291ed9a2c51d9fef6a7
Penn Bauman <pennbauman@protonmail.com>
2020-07-23 00:49:12 -0400

    setting morse code fonts by browser size

--- projects/morse-code.php
+++ projects/morse-code.php
@@ -2,29 +2,28 @@
 	<head>
 		<title>Morse Code - Penn Bauman</title>
 		<meta charset='UTF-8'>
-		<meta name='description' content="Simple morse code typer."/>
+		<meta name='viewport' content='width=device-width, initial-scale=1.0'>
+		<meta name='description' content='Simple morse code typer.'/>
 		<meta name='author' content='Penn Bauman'>
 		<meta name='robots' content='noindex, nofollow'/>
-		<link rel="shortcut icon" href="/files/img/morse_favicon.png"> 
-		<link rel='stylesheet' type='text/css' href='/files/css/morse-code.css'>
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+		<link rel='icon' href='/files/img/morse_favicon.png'>
+		<link rel='stylesheet' href='/files/css/morse-code.css'>
+		<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js'></script>
 		<script src='/files/js/morse-code.js'></script>
 	</head>
-
 	<body>
 		<div id="back">
-			<a class="hl" href="/projects/">Back</a>
-			<span style="color:black">X</span>
-			<a class="hl" href="/">Home</a>
+			<a href="/projects/">Back</a>
+			x
+			<a href="/">Home</a>
 		</div>
 		<div id="clear">
-			<span class="hl" onclick="htmlPrint('main', ''); htmlPrint('bottom', ''); return false;">Clear</span>
+			<a onclick="htmlPrint('main', ''); htmlPrint('bottom', ''); return false;">Clear</a>
 		</div>
 		<div id="header">
-			MORSE CODE<br/>
-			<span style="font-size:3.2520325vh">-- --- .-. ... . / -.-. --- -.. .</span>
+			<h1>MORSE CODE</h1>
+			<i>-- --- .-. ... . / -.-. --- -.. .</i>
 		</div>
-		<div id="back_head"> </div>
 		<div id="main"> </div>
 		<div id="bottom"> </div>
 	</body>
