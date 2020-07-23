63e3f66bcd5004df5965f66741209bbbc78c761a
Penn Bauman <pennbauman@protonmail.com>
2020-07-23 00:57:02 -0400

    setting monte carlo fonts by browser size

--- projects/monte-carlo.php
+++ projects/monte-carlo.php
@@ -2,12 +2,13 @@
 	<head>
 		<title>Monte Carlo PI Generator - Penn Bauman</title>
 		<meta charset='UTF-8'>
-		<meta name='description' content="Javascript Monte Carlo simulation of pi."/>
+		<meta name='viewport' content='width=device-width, initial-scale=1.0'>
+		<meta name='description' content='Javascript Monte Carlo simulation of pi.'/>
 		<meta name='author' content='Penn Bauman'>
 		<meta name='robots' content='noindex, nofollow'/>
-		<link rel="shortcut icon" href="/files/img/pi_favicon.png"> 
-		<link rel='stylesheet' type='text/css' href='/files/css/monte-carlo.css'>
-		<script type="text/javascript" src="/files/js/monte-carlo.js"></script>
+		<link rel='shortcut icon' href='/files/img/pi_favicon.png'>
+		<link rel='stylesheet' href='/files/css/monte-carlo.css'>
+		<script src='/files/js/monte-carlo.js'></script>
 	</head>
 	<body onload="setup();return false;">
 		<canvas id="draw"></canvas>
@@ -17,13 +18,10 @@
 				<b>PI: </b><span id="pi">0.0000000000000000</span> <br/>
 				<b>Points Used: </b><span id="n_points">0</span> <br/>
 			</p>
-			<div id="spacer"></div>
-			<table id="table">
-				<tr>
-					<th> <div onclick="runState()" id="run_button" class="button">RUN</div> </th>
-					<th> <div onclick="reset()" class="button">RESET</div> </th>
-				</tr>
-			</table>
+			<div id="buttons">
+				<div onclick="runState()" id="run_button" class="button">RUN</div>
+				<div onclick="reset()" class="button">RESET</div>
+			</div>
 			<p id="explination">
 				The Monte Carlo method for calculating pi uses the proportion of points within a circle to the points within its curcumscribed square. This proportion is approximated using points that are randomly generated in the square, and then their distance from the center of the circle is calculated to determine if they are in the circle. This proportion becomes more accurate the more points are calculated. 
 				<br/><br/>
