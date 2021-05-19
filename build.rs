2c0ab732e49041fae5713496bd72128e7a3bfbfd
Penn Bauman <pennbauman@protonmail.com>
2021-05-19 04:15:03 -0400

    cleaned up sass compile

---
+++ build.rs
@@ -0,0 +1,20 @@
+// pennbauman.com
+//   URL: https://github.com/pennbauman/pennbauman.com
+//   License: GPLv3.0
+//   Author:
+//     Penn Bauman (pennbauman@protonmail.com)
+use std::fs::File;
+use std::io::Write;
+use sass_rs::{Options, compile_file};
+
+fn main() {
+    // SASS: sass/general.scss -> files/css/general.css
+    println!("cargo:rerun-if-changed=sass/general.scss");
+    match compile_file("sass/general.scss", Options::default()) {
+        Ok(s) => {
+            let mut f = File::create("files/css/general.css").expect("open failed");
+            f.write_all(s.as_bytes()).expect("write failed");
+        },
+        Err(s) => println!("cargo:warning=Sass: {}", s),
+    };
+}
