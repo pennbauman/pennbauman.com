d4da07c46c0ca52ea66ef5f6d1621a09b64a59c4
Penn Bauman <pennbauman@protonmail.com>
2021-05-19 03:41:11 -0400

    removed all but core pages

--- src/main.rs
+++ src/main.rs
@@ -3,64 +3,57 @@
 //   License: GPLv3.0
 //   Author:
 //     Penn Bauman (pennbauman@protonmail.com)
-use tide::{Response, StatusCode};
+use std::env;
+use tide::{Request, Response, Redirect, StatusCode};
 use tide::utils::After;
-use tide_fluent_routes::prelude::*;
-use mongodb::{Client, options::ClientOptions, Database};
+use tide_tera::prelude::*;
 use tera::Tera;
-
-mod apps;
-mod config;
-
-#[derive(Clone)]
-pub struct State {
-    tera: Tera,
-    db: Database,
-}
-impl State {
-    async fn new() -> Result<State, tide::Error> {
-        let template_dir: String = match config::path("templates/**/*").await {
-            Ok(s) => s,
-            Err(_) => return Err(tide::Error::from_str(500, "broken template path")),
-        };
-
-        let db_options = ClientOptions::parse(&config::db().await?).await?;
-        let client = Client::with_options(db_options)?;
-
-        Ok(State {
-            tera: Tera::new(&template_dir)?,
-            db: client.database("pennbauman"),
-        })
-    }
-}
 
 #[async_std::main]
 async fn main() -> tide::Result<()> {
-    tide::log::start();
-    let mut app = tide::with_state(State::new().await?);
+    // Get directories
+    let dir: String = env::var("TIDE_DIR")?;
+    let templates_dir = dir.to_string() + "/templates/*";
 
-    // Print basic error messages
+    tide::log::start();
+    let mut app = tide::with_state(Tera::new(&templates_dir)?);
+    app.at("/files").serve_dir(dir + "/files")?;
+
+    // Print error codes
     app.with(After(|mut res: Response| async {
         match res.status() {
             StatusCode::Ok => Ok(res),
             _ => {
                 let num: u16 = res.status().into();
-                res.set_body(format!("{} : {}\n", num, res.status().canonical_reason()));
+                res.set_body(format!("{}: {}\n", num, res.status().canonical_reason()));
                 Ok(res)
             },
         }
     }));
 
-    // Apps
-    app.register(root()
-        .at("", apps::core)
-        .at("dnd", apps::dnd)
-        .at("", apps::links)
-    );
+    // Pages
+    app.at("/").get(|req: Request<Tera>| async move {
+        let tera = req.state();
+        tera.render_response("index.html", &context!{})
+    });
+    app.at("/about").get(|req: Request<Tera>| async move {
+        let tera = req.state();
+        tera.render_response("about.html", &context!{})
+    });
+    app.at("/site").get(|req: Request<Tera>| async move {
+        let tera = req.state();
+        tera.render_response("site.html", &context!{})
+    });
 
-    // Basic Server
-    app.at(&config::files_path("/").await).serve_dir(&config::files_dir().await?)?;
-    app.listen("localhost:8080").await?;
+    app.at("/github").get(|_req: Request<Tera>| async {
+        Ok(Redirect::new("http://github.com/pennbauman"))
+    });
+    app.at("/resume").get(|_req: Request<Tera>| async {
+        Ok(Redirect::new("/files/Penn_Bauman_Resume.pdf"))
+    });
+
+    // Run Server
+    app.listen("0.0.0.0:8080").await?;
 
     Ok(())
 }
